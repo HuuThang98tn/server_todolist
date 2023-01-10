adc9a3e7934582b52aca200aa3955b55b95784be
Huuthang98tn <tranhuuthang98fpttn@gmail.com>
2023-01-10 20:46:58 +0700

    f

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connect() {
     try {
         await mongoose.set("strictQuery", false)
-        await mongoose.connect(process.env.DATABASE_URL, {
+        await mongoose.connect("mongodb://127.0.0.1:27017/todolist", {
             // useNewUrlParser: true,
             // useUnifiedTopology: true,
             // useFindAndModify: false,
