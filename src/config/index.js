7c937796b56d21a886993be3206c59d5b227f379
Huuthang98tn <tranhuuthang98fpttn@gmail.com>
2023-01-10 19:50:06 +0700

    f

--- src/config/index.js
+++ src/config/index.js
@@ -1,22 +1,42 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+
+// async function connect() {
+//     try {
+//         await mongoose.set("strictQuery", false)
+//         await mongoose.connect(process.env.DATABASE_URL, {
+//             // useNewUrlParser: true,
+//             // useUnifiedTopology: true,
+//             // useFindAndModify: false,
+//             // useCreateIndex: true,
+//             // retryWrites: true,
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+
+//         console.log('Connect success');
+//     } catch (error) {
+//         console.log('Connect failed');
+//     }
+// }
+
+// module.exports = { connect };
+
+const mMONGOOSE = require("mongoose");
 
 async function connect() {
     try {
-        await mongoose.set("strictQuery", false)
-        await mongoose.connect(process.env.DATABASE_URL, {
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true,
-            // useFindAndModify: false,
-            // useCreateIndex: true,
-            // retryWrites: true,
+        const connectionParams = {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        });
+        };
+        await mMONGOOSE.connect(process.env.DATABASE_URL, connectionParams);
 
-        console.log('Connect success');
+        console.log("Kết nối cơ sở dữ liệu thành công !!!");
+
     } catch (error) {
-        console.log('Connect failed');
+        console.log(error);
+        console.log("Kết nối cơ sở dữ liệu thất bại !!!");
     }
 }
-
 module.exports = { connect };
+
