055e9af47c7e8c2ea8fda6f81c6033cad30df363
Tran Huu Thang <tranhuuthang98fpttn@gmail.com>
2023-01-11 09:39:02 +0700

    port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express')
 const app = express()
-const port = 80
+const port = 8080
 const cors = require('cors');
 const db = require('../src/config');
 const routes = require("../src/routes")
